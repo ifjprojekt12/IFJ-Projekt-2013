744b78d5f4285f20f82141560091114bf730c56c
Fiser33 <Fiser33@seznam.cz>
2013-10-03 15:49:09 +0200

    syntax v2.0

--- syntax.c
+++ syntax.c
@@ -2,109 +2,240 @@
 
 int syntaxer()
 {
-	//pomocna promenna
-	TOKEN unit;
-	// vytvoreni pole tokenu a jeho inicializace
-	TOKEN array[512];	// 512 - magicke cislo, nutno prepsat (I_MAX)
-	initialize_array(array);
+    //pomocna promenna
+    TOKEN unit;
+    // vytvoreni pole tokenu a jeho inicializace
+    TOKEN array[I_MAX];
+    initialize_array(array);
 
-	unit = get_token();
-	int i = 0;
-	int brackets = 0;
-	while(unit.type_token > 0 && unit.type_token != 11 )	// konec pri chybe nebo EOF (ted pri znaku '-')
-	{
-		array[i++] = unit;
+    unit = get_token();
+    int i = 0;
+    int brackets = 0;
+    int super_brackets = 0;
+    while(unit.type_token > 0 && unit.type_token != 50 )    // konec pri chybe nebo EOF
+    {
+        array[i++] = unit;
+        if( i == I_MAX )
+        {
+            printf("Dosazena maximalni velikost pole tokenu.\n");
+            break;
+        }
 
-		// if, elseif, while, for
-		if( unit.type_token == 1 || unit.type_token == 3 || unit.type_token == 4 || unit.type_token == 5 )
-		{
-			unit = get_token();
-			array[i++] = unit;
-			if( unit.type_token == 40 ) // (
-			{
-				if( (i = expression(array, i)) < 0 )
-				{
-					// TODO chyba
-				}
-				unit = get_token();
-				array[i++] = unit;
-				if( unit.type_token == 41 )	// )
-				{
-					unit = get_token();
-					array[i++] = unit;
-					if( unit.type_token == 42 )	// {
-					{
-						brackets++;
-					}
-				}
-			}
-		}
-		else if( unit.type_token == 43 )	// }
-		{
-			brackets--;
-			if( brackets < 0 )
-			{
-				printf("prilis mnoho uzaviracich zavorek\n");
-				// TODO chyba
-			}
-		}
-	
-		unit = get_token();	// nacteni dalsiho tokenu
-	}
-	array[i] = unit;
+        // if, elseif, while
+        if( unit.type_token == 1 || unit.type_token == 3 || unit.type_token == 4 )
+        {
+            unit = get_token();
+            array[i++] = unit;
+            if( i == I_MAX )
+            {
+                printf("Dosazena maximalni velikost pole tokenu.\n");
+                break;
+            }
 
-	if( brackets != 0 )
-	{
-		printf("nespravny pocet zavorek\n");
-		// TODO chyba
-	}
+            if( unit.type_token == 40 ) // (
+            {
+                if( (i = expression(array, i, get_token(), END_B)) < 0 )
+                {
+                    printf("chyba\n");
+                    break;
+                    // TODO chyba
+                }
+                unit = get_token();
+                array[i++] = unit;
+                if( i == I_MAX )
+                {
+                    printf("Dosazena maximalni velikost pole tokenu.\n");
+                    break;
+                }
 
-	// vypis pole tokenu
-	for(int x=0; x<512; x++)
-	{
-		if(array[x].type_token == 0)	break;
-		printf("%d ", array[x].type_token);
-	}
-	printf("\n");
+                if( unit.type_token == 42 ) // {
+                {
+                    super_brackets++;
+                }
+                else
+                {
+                    // TODO chyba
+                    printf("Chybi slozena oteviraci zavorka.\n");
+                    break;
+                }
+            }
+            else
+            {
+                // TODO chyba
+                printf("chyba\n");
+                break;
+            }
+        }
+        else if( unit.type_token == 7 )     // return
+        {
+            if( (i = expression(array, i, get_token(), END_S)) < 0)
+            {
+                printf("chyba\n");
+                break;
+                // TODO chyba
+            }
+        }
+        else if( unit.type_token == 2 )   // else
+        {
+            unit = get_token();
+            array[i++] = unit;
+            if( i == I_MAX )
+            {
+                // TODO chyba
+                printf("Dosazena maximalni velikost pole tokenu.\n");
+                break;
+            }
 
-	return EXIT_SUCCESS;
+            if( unit.type_token == 42 ) // {
+            {
+                super_brackets++;
+            }
+            else
+            {
+                // TODO chyba
+                printf("Chybi slozena oteviraci zavorka.\n");
+                break;
+            }
+        }
+        else if( unit.type_token == 36 )    // promenna
+        {
+            unit = get_token();
+            array[i++] = unit;
+            if( i == I_MAX )
+            { 
+                printf("Dosazena maximalni velikost pole tokenu.\n");
+                break;
+            }
+
+            if( unit.type_token == 10 )     // =
+            {
+                if( (i = expression(array, i, get_token(), END_S)) < 0 )
+                {
+                    // TODO chyba
+                    printf("chyba\n");
+                    break;
+                }
+            }
+            else
+            {
+                // TODO chyba
+                printf("chyba\n");
+                break;
+            }
+        }
+        else if( unit.type_token == 6 )
+        else if( unit.type_token == 43 )    // }
+        {
+            super_brackets--;
+            if( super_brackets < 0 )
+            {
+                printf("prilis mnoho slozenych uzaviracich zavorek\n");
+                break;
+                // TODO chyba
+            }
+        }
+        else
+        {
+            // TODO chyba
+            printf("chyba\n");
+            break;
+        }
+    
+        unit = get_token(); // nacteni dalsiho tokenu
+    }
+    array[i] = unit;
+
+    if( brackets != 0 && super_brackets != 0 )
+    {
+        printf("nespravny pocet zavorek\n");
+        // TODO chyba
+    }
+
+    // vypis pole tokenu
+    for(int x=0; x<I_MAX; x++)
+    {
+        if(array[x].type_token == 0)    break;
+        printf("%d ", array[x].type_token);
+    }
+    printf("\n");
+
+    return EXIT_SUCCESS;
 }
 
 // funkce pro inicializaci pole tokenu
 void initialize_array(TOKEN*array)
 {
-	for(int x=0; x<512; x++)
-	{
-		token_init(&array[x]);
-	}
+    for(int x=0; x<512; x++)
+    {
+        token_init(&array[x]);
+    }
 }
 
 // funkce pro kontrolu vyrazu
-int expression(TOKEN*array, int i)
+int expression(TOKEN*array, int i, TOKEN unit, int ending)
 {
-	bool isFirst = true;
-	bool wasExpr = false;
-	bool wasSign = false;
-	int brackets = 0;
+//    bool isFirst = true;
+    bool wasExpr = false;
+    bool wasSign = true;
+    int brackets = 0;
 
-	TOKEN unit = get_token();
+    while( unit.type_token > 0 && unit.type_token != 50 && unit.type_token != 100 ) // nic, EOF, chyba
+    {
+        array[i++] = unit;
+        if( i == I_MAX )
+        {
+            printf("Dosazena maximalni velikost pole tokenu.\n");
+            break;
+        }
 
-	while( unit.type_token > 0 && unit.type_token != 50 && unit.type_token != 100 )	// nic, EOF, chyba
-	{
-		array[i++] = unit;
-		if( isFirst && unit.type_token == 40 )	// (
-		{
-			brackets++;
-		}
-		else if( isFirst && unit.type_token >= 30 && unit.type_token <= 36 )	// string, int, double, boolean, null, id, $id
-		{
-			isFirst = false;
-			wasExpr = true;
-			wasSign = false;
-		}
+        if( wasSign && unit.type_token == 40 )  // (
+        {
+            brackets++;
+        }
+        else if( wasSign && unit.type_token >= 30 && unit.type_token <= 36 )    // string, int, double, boolean, null, id, $id
+        {
+            wasExpr = true;
+            wasSign = false;
+        }
+        else if( wasExpr && unit.type_token > 10 && unit.type_token <= 21 )     // -, *, /, +, >, <, >=, <=, ===, !==, .
+        {
+            wasExpr = false;
+            wasSign = true;
+        }
+        else if( wasExpr && unit.type_token == 41 )    // )
+        {
+            brackets--;
+            if( brackets < 0 )
+            {
+                if( ending != END_S )
+                    break;
+                else
+                {
+                    // TODO chyba!!
+                    printf("chyba\n");
+                    return -1;
+                }
+            }
+        }
+        else if( ending == END_S && wasExpr && unit.type_token == 22 )     // ;
+        {
+            if( brackets != 0 )
+            {
+                // TODO chyba
+                printf("chybi zavorka ve vyrazu.\n");
+                return -1;
+            }
+            break;
+        }
+        else
+        {
+            // TODO chyba
+            printf("Znak na nespravnem miste.\n");
+            return -1;
+        }
 
-		if( unit.type_token
-	}
+        unit = get_token();
+    }
 
-	return i;
+    return i;
 }
--- syntax.h
+++ syntax.h
@@ -6,8 +6,21 @@
 #include <stdbool.h>
 #include "lexical.h"
 
+#define OK      0
+#define END_B   30
+#define END_S   31
+#define END_CB  32
+#define END_SB  33
+#define I_MAX   512
+
 int syntaxer();
 void initialize_array(TOKEN*array);
-int expression(TOKEN*array, int i);
+int expression(TOKEN*array, int i, TOKEN unit, int ending); // ending - urcuje, jaky znak ocekavame na konci vyrazu
+/*
+    END_B = 30  (bracket)
+    END_S = 31  (semicolon)
+    END_CB = 32 (comma, bracket)
+    END_SB = 33 (semicolon, bracket)
+*/
 
 #endif
