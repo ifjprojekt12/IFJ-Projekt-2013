6d80af5bfbee1a47fb80451bfb80e8a5345a69ce
Fiser33 <Fiser33@seznam.cz>
2013-10-27 23:10:18 +0100

    prepsano chybne ukazovani do struktury TOKEN

--- ial.c
+++ ial.c
@@ -102,18 +102,18 @@ NODE copyTree(NODE *ptr, NODE *ptrNew)
     {
         treeNew->key = malloc((strlen(tree->key)+1) * sizeof(char));
         strcpy(treeNew->key, tree->key);
-        treeNew->data->type_token = tree->data->type_token;
-        treeNew->data->c_number = tree->data->c_number;
-        treeNew->data->d_number = tree->data->d_number;
-        if (tree->data->string != NULL)
+        treeNew->data.type_token = tree->data.type_token;
+        treeNew->data.c_number = tree->data.c_number;
+        treeNew->data.d_number = tree->data.d_number;
+        if (tree->data.string != NULL)
         {
-            treeNew->data->string = malloc((strlen(tree->data->string)+1) * sizeof(char));
-            strcpy(treeNew->data->string, tree->data->string);
+            treeNew->data.string = malloc((strlen(tree->data.string)+1) * sizeof(char));
+            strcpy(treeNew->data.string, tree->data.string);
         }
-        treeNew->data->boolean = tree->data->boolean;
-        treeNew->data->null = tree->data->null;
-        treeNew->data->id_name = malloc((strlen(tree->data->id_name)+1) * sizeof(char));
-        strcpy(treeNew->data->id_name, tree->data->id_name);
+        treeNew->data.boolean = tree->data.boolean;
+        treeNew->data.null = tree->data.null;
+        treeNew->data.id_name = malloc((strlen(tree->data.id_name)+1) * sizeof(char));
+        strcpy(treeNew->data.id_name, tree->data.id_name);
 
         if (tree->LPtr != NULL) treeNew->LPtr = copyTree(&tree->LPtr, &treeNew->LPtr);
         else treeNew->LPtr = NULL;
