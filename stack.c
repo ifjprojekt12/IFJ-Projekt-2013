fa1373192c2f4f3de0b09141525cf3e455cbb6b6
Fiser33 <Fiser33@seznam.cz>
2013-10-28 04:34:47 +0100

    pridan zasobnik ukazatelu na instrukce krome funkce Dispose

--- stack.c
+++ stack.c
@@ -1,110 +1,138 @@
 #include "stack.h"
 
-void init (tStack *S)	{
-	S->Last = NULL;
+void init (tStack *S)   {
+    S->Last = NULL;
 }
 
-void DisposeStack (tStack *S)	{
-	tElemPtr ptr = NULL;
-	tElemPtr Del = NULL;
-	ptr = S->Last;
-	while (ptr != NULL)
-	{
-		Del = ptr;
-		ptr = ptr->ptr;
-		free(Del);
-	}
-	S->Last = NULL;
+void DisposeStack (tStack *S)   {
+    tElemPtr ptr = NULL;
+    tElemPtr Del = NULL;
+    ptr = S->Last;
+    while (ptr != NULL)
+    {
+        Del = ptr;
+        ptr = ptr->ptr;
+        free(Del);
+    }
+    S->Last = NULL;
 }
 
-void PUSH (tStack *S, int val)	{
-    	tElemPtr ptr = NULL;
-	ptr =  malloc(sizeof(struct tElem));
-	ptr->Elem = val;
-	ptr->ptr=S->Last;
-	S->Last = ptr;
+void PUSH (tStack *S, int val)  {
+        tElemPtr ptr = NULL;
+    ptr =  malloc(sizeof(struct tElem));
+    ptr->Elem = val;
+    ptr->ptr=S->Last;
+    S->Last = ptr;
 }
 
-void TOP (tStack *S, int *val)		{
-		*val = S->Last->Elem;
+void TOP (tStack *S, int *val)      {
+        *val = S->Last->Elem;
 }
 
 int TOPCheck(tStack *S)     {
     return S->Last->Elem;
 }
 
-void POP (tStack *S)	{
+void POP (tStack *S)    {
     tElemPtr ptr = NULL;
-	ptr = S->Last;
-	S->Last = S->Last->ptr;
-	free(ptr);
+    ptr = S->Last;
+    S->Last = S->Last->ptr;
+    free(ptr);
 }
 
 bool SEmpty(tStack *S){
-	if (S->Last == NULL){
-		return true;
-	}else{
-		return false;
-	}
+    if (S->Last == NULL){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 void initNode(tSNode *S){
-	S->Last = NULL;
+    S->Last = NULL;
 }
 
-/*void DisposeSNode (tSNode *S)	{
-	tNodePtr ptr = NULL;
-	tNodePtr Del = NULL;
-	ptr = S->Last;
-	while (ptr != NULL)
-	{
-		Del = ptr;
-		ptr = ptr->ptr;
-		free(Del);
-	}
-	S->Last = NULL;
+/*void DisposeSNode (tSNode *S) {
+    tNodePtr ptr = NULL;
+    tNodePtr Del = NULL;
+    ptr = S->Last;
+    while (ptr != NULL)
+    {
+        Del = ptr;
+        ptr = ptr->ptr;
+        free(Del);
+    }
+    S->Last = NULL;
 }*/
 
-void PUSHNode (tSNode *S, NODE val)	{
-    	tNodePtr ptr = NULL;
-	ptr = malloc(sizeof(struct tNode));
-	ptr->Node = val;
-	ptr->ptr=S->Last;
-	S->Last = ptr;
+void PUSHNode (tSNode *S, NODE val) {
+        tNodePtr ptr = NULL;
+    ptr = malloc(sizeof(struct tNode));
+    ptr->Node = val;
+    ptr->ptr=S->Last;
+    S->Last = ptr;
 }
 
-void TOPNode (tSNode *S, NODE *val)		{
-		*val = S->Last->Node;
+void TOPNode (tSNode *S, NODE *val)     {
+        *val = S->Last->Node;
 }
 
-void POPNode (tSNode *S)	{
+void POPNode (tSNode *S)    {
     tNodePtr ptr = NULL;
-	ptr = S->Last;
-	S->Last = S->Last->ptr;
-	free(ptr);
+    ptr = S->Last;
+    S->Last = S->Last->ptr;
+    free(ptr);
 }
 
 void TOPPOPNode(tSNode *S, NODE *val){
-	*val = S->Last->Node;
-	tNodePtr ptr = NULL;
-	ptr = S->Last;
-	S->Last = S->Last->ptr;
-	free(ptr);
+    *val = S->Last->Node;
+    tNodePtr ptr = NULL;
+    ptr = S->Last;
+    S->Last = S->Last->ptr;
+    free(ptr);
 }
 
 /*NODE* ToPoPtr(tSNode *){
-	NODE *val = S->Last->Node;
-	tSNodePtr ptr = NULL;
-	ptr = S->Last;
-	S->Last = S->Last->ptr;
-	//free(ptr);
-	return val;
+    NODE *val = S->Last->Node;
+    tSNodePtr ptr = NULL;
+    ptr = S->Last;
+    S->Last = S->Last->ptr;
+    //free(ptr);
+    return val;
 }*/
 
 bool SEmptyNode(tSNode *S){
-	if (S->Last == NULL){
-		return true;
-	}else{
-		return false;
-	}
+    if (S->Last == NULL){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+void initInstr(tSInstr *S)
+{
+    S->Last = NULL;
+}
+
+void PUSHInstr(tSInstr *S, INSTRUCT val)
+{
+    tInstrPtr ptr = NULL;
+    ptr = malloc(sizeof(struct tInstr));
+    ptr->Instr = val;
+    ptr->ptr=S->Last;
+    S->Last = ptr;
+}
+
+void POPInstr(tSInstr *S, INSTRUCT *val)
+{
+    *val = S->Last->Instr;
+    tInstrPtr ptr = NULL;
+    ptr = S->Last;
+    S->Last = S->Last->ptr;
+    free(ptr);
+}
+
+bool SEmptyInstr(tSInstr *S)
+{
+    return (S->Last == NULL ? true : false);
 }
--- stack.h
+++ stack.h
@@ -9,41 +9,56 @@
 
 #include "lexical.h"
 #include "ial.h"
+#include "data_struct.h"
 
 typedef struct tElem {
-    struct tElem *ptr;	//ukazatel na predchozi prvek
-    int Elem;			//hodnota na zasobniku
+    struct tElem *ptr;  //ukazatel na predchozi prvek
+    int Elem;           //hodnota na zasobniku
 } *tElemPtr;
 
 typedef struct { 
-	tElemPtr Last;
+    tElemPtr Last;
 } tStack;
 
 typedef struct tNode {
-    struct tNode *ptr;	//ukazatel na predchozi prvek
-    NODE Node;			//hodnota na zasobniku
+    struct tNode *ptr;  //ukazatel na predchozi prvek
+    NODE Node;          //hodnota na zasobniku
 } *tNodePtr;
 
 typedef struct { 
-	tNodePtr Last;
+    tNodePtr Last;
 } tSNode;
 
+typedef struct tInstr {
+    struct tInstr *ptr; //ukazatel na predchozi prvek
+    INSTRUCT Instr;  //hodnota na zasobniku
+} *tInstrPtr;
 
-void initNode(tSNode *);	//Inicializace zasobniku
-void PUSHNode(tSNode *,NODE);	// Vlozi na vrchol zasobniku El
-void POPNode(tSNode *);		// Uvolni posledni prvek na zasobniku
-void TOPNode(tSNode *,NODE *);		// Vrati prvek z vrcholu zasobniku
+typedef struct {
+    tInstrPtr Last;
+} tSInstr;
+
+
+void initNode(tSNode *);    //Inicializace zasobniku
+void PUSHNode(tSNode *,NODE);   // Vlozi na vrchol zasobniku El
+void POPNode(tSNode *);     // Uvolni posledni prvek na zasobniku
+void TOPNode(tSNode *,NODE *);      // Vrati prvek z vrcholu zasobniku
 void TOPPOPNode(tSNode *, NODE *);
-void DisposeSNode(tSNode *);	//Zrusi zasobnik
-bool SEmptyNode(tSNode *);		// Je zasobnik prazdny
+void DisposeSNode(tSNode *);    //Zrusi zasobnik
+bool SEmptyNode(tSNode *);      // Je zasobnik prazdny
 
 
-void init(tStack *);	//Inicializace zasobniku
-void PUSH(tStack *,int);	// Vlozi na vrchol zasobniku El
-void POP(tStack *);		// Uvolni posledni prvek na zasobniku
-void TOP(tStack *,int *);		// Vrati prvek z vrcholu zasobniku
-int TOPCheck(tStack *);		// vrati prvek z vrcholu zasobniku
-void DisposeStack (tStack *);	//Zrusi zasobnik
-bool SEmpty(tStack *);		// Je zasobnik prazdny
+void init(tStack *);    //Inicializace zasobniku
+void PUSH(tStack *,int);    // Vlozi na vrchol zasobniku El
+void POP(tStack *);     // Uvolni posledni prvek na zasobniku
+void TOP(tStack *,int *);       // Vrati prvek z vrcholu zasobniku
+int TOPCheck(tStack *);     // vrati prvek z vrcholu zasobniku
+void DisposeStack (tStack *);   //Zrusi zasobnik
+bool SEmpty(tStack *);      // Je zasobnik prazdny
+
+void initInstr(tSInstr *);
+void PUSHInstr(tSInstr *, INSTRUCT);
+void POPInstr(tSInstr *, INSTRUCT *);
+bool SEmptyInstr(tSInstr *);
 
 #endif
