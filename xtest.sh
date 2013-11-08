b6119976fe37762857646a2d1db057b8cb400a87
HanzCZ <Honzales@gmail.com>
2013-11-08 12:23:15 +0100

    Create xtest.sh
    testovaci skript. navod k pouziti viz prvni radky

---
+++ xtest.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+#Pouziti:
+#vytvor testovaci soubor a vloz ho do ./testy/{JMENOPROGRAMU}
+#{JMENOPROGRAMU} by melo popsat co program testuje
+#vytvor ocekavany vystup tohoto programu a vloz ho do ./testy/out/{JMENOPROGRAMU}Out
+#vytvor ocekavany navratovy kod k tomuto programu a vloz ./testy/out/{JMENOPROGRAMU}Ecode
+#opakuj pro libovolny pocet programu IFJ13
+
+
+res=0;
+
+
+#barvy
+green='\e[0;32m'
+red='\e[0;31m'
+lred='\e[1;31m'
+yellow='\e[1;33m'
+NC='\e[0m' # no color
+
+
+#ziskani testovacich souboru
+location="./testy"
+foundExp=`find ./ -maxdepth 1 -type f ! -name '.*'` 
+foundExp=$(echo "$foundExp" | sed "s|./||g")
+
+foundProj=`find $location -maxdepth 1 -type f ! -name '.*'`
+foundProj=`echo "$foundProj" | rev | cut -d"/" -f1-1 | rev`
+	
+n1=$'\n'
+found="${foundExp}${n1}${foundProj}"
+found=$(echo  "$found" | sort -u)
+echo "$found" > ./testy/.files
+
+
+#testovani pokud jsou nalezeny testy a vysledky k nim
+CT=1; #cislo testu
+cat ./testy/.files | while read line; do
+	if [ -f "./testy/${line}" ] && [ -f "./testy/out/${line}Out" ]
+	then
+		./interpret testy/${line} > ./testy/.out
+		echo $? > ./testy/.ecode 
+		diff -u "./testy/.out" "./testy/out/${line}Out" >/dev/null 2> /dev/null
+		if [ $? -eq 0 ]
+		then 	echo -e "TEST ${CT}  OUTPUT.: ${green}SUCCESS${NC} --- ${yellow}${line}${NC}"
+
+		else
+			echo -e "TEST ${CT}  OUTPUT.: ${red} FAIL  ${NC} --- ${yellow}${line}${NC}"
+		fi	
+
+		diff -u "./testy/.ecode" "./testy/out/${line}Ecode" > /dev/null 2> /dev/null
+		if [ $? -eq 0 ]
+		then 	echo -e "TEST ${CT}  ECODE .: ${green}SUCCESS${NC} --- ${yellow}${line}${NC}"
+		else
+			echo -e "TEST ${CT}  ECODE .: ${red} FAIL  ${NC} --- ${yellow}${line}${NC}"
+		fi	
+
+		CT=$((CT+1));  # inkrementace
+	fi	
+	
+done
+
+
+rm ./testy/.out 2> /dev/null > /dev/null
+rm ./testy/.files 2> /dev/null > /dev/null
+rm ./testy/.ecode 2> /dev/null > /dev/null
+exit 0
