9ad1c7d455ef0d0dea76dadca53a365d5cbf7acc
Michal Flax <maxxa.sns@gmail.com>
2013-11-14 13:12:55 +0100

    tak nakonec spis duplikace...

--- lexical.c
+++ lexical.c
@@ -190,6 +190,22 @@ TOKEN get_token(){
     }
   }
 
+  //prebytecna bila mista
+  while(1){
+    if((isspace(buffer[pos_buffer])) != 0 && buffer[pos_buffer] != '\n'){
+      pos_buffer++;
+      //a toto nacte novy radek, pokud jsou prebytecna bila mista na konci radku
+      if(buffer[pos_buffer] == '\n'){
+        if(read_src() == 1){
+          new_tok.type_token = 50;
+          return new_tok;
+        }
+        pos_buffer = 0;
+      }
+
+    } else break;
+  }
+
   //-------------------------------------------------
   //konecny automat...
   //-------------------------------------------------
